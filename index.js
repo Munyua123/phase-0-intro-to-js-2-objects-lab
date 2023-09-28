aff9551e82293b21e2c942127962b5ca53a2e83c
Earl <earlmunyua96@gmail.com>
2023-09-28 18:19:13 +0300

    Java script object

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+let employee = {
+    name : "Ham",
+    streetAddress: "10 Broadway"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updateEmployee = {...employee, 'streetAddress': '11 Broadway'}
+    return updateEmployee
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee["streetAddress"] = "12 Broadway"
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key) {
+   const newEmployee = {employee, "streetAddress" : ""}
+   return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {employee, "streetAddress" : ""} 
+    delete employee[key]
+    return employee
+     return newEmployee
+}
